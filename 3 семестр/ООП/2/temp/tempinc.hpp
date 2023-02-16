549028c986920f1165c71f45f7030441658a6af9
maximsavonin <57619285+maximsavonin@users.noreply.github.com>
2023-02-16 21:51:01 +0500

    Delete 3 семестр/ООП directory

--- 3 семестр/ООП/2/temp/tempinc.hpp
+++
@@ -1,3 +0,0 @@
-struct _abracadabra_type {};
-int _abracadabra_cast(_abracadabra_type);
-#include <iostream>
--- 3 семестр/ООП/Курсовая/7/temp/tempinc.h
+++
@@ -1,4 +0,0 @@
-struct _abracadabra_type {};
-typedef struct _abracadabra_type _abracadabra_type;
-int _abracadabra_cast(_abracadabra_type);
-#include <vector>
